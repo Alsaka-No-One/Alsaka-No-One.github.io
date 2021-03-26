b9ed08ed988ad221d990a004750722db85029763
Alain NGUYEN <50915198+Alsaka-No-One@users.noreply.github.com>
2021-03-26 21:06:56 +0100

    [Modif] index.js
    Adding the searchbar for the projects section

--- js/index.js
+++ js/index.js
@@ -11,3 +11,22 @@ document.addEventListener("scroll", () => {
         navBar.classList.remove("fixed-top");
     }
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    var input = document.getElementById("search-input");
+
+    input.addEventListener("input", (event) => {
+        let value = event.target.value
+            .toLocaleLowerCase()
+            .split(" ")
+            .filter(Boolean);
+
+        document.querySelectorAll(".card-white").forEach(el => {
+            let titleElement = el.querySelector('h2').textContent.toLocaleLowerCase();
+            
+            el.style.display = !value.length || value.some(
+                val => titleElement.includes(val)
+            ) ? "" : "none";
+        });
+    });
+});
